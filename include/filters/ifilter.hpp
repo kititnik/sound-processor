719b4d086df0699243becedf0778031d39de3074
kititnik <kititnik@gmail.com>
2026-06-10 11:41:47 +0300

    Rule of 5 for IFilter

--- include/filters/ifilter.hpp
+++ include/filters/ifilter.hpp
@@ -4,6 +4,9 @@
 
 class IFilter {
 public:
-    virtual ~IFilter() {}
+    IFilter() = default;
+    virtual ~IFilter() = default;
+    IFilter(const IFilter&) = delete;
+    IFilter& operator=(const IFilter&) = delete;
     virtual void apply(Waveform* sound) = 0;
 };
